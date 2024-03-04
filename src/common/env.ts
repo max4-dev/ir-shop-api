c843c1c5993d4b748d6a399f1a056e4b7d51c693
Max <maksim.bogomyakov@gmail.com>
2024-03-04 08:07:31 +0300

    added database

---
+++ src/common/env.ts
@@ -0,0 +1,3 @@
+export default () => ({
+  database: process.env.MONGODB_CONNECTION,
+});
---
+++ src/mongo/mongo.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import env from '../common/env';
+
+const envModule = ConfigModule.forRoot({
+  isGlobal: true,
+})
+
+@Module({
+  imports: [MongooseModule.forRoot(env().database)],
+})
+
+export class MongoModule {}
