c52cbd39f076f3dc86b810411b43870ea9bce3b3
Max <maksim.bogomyakov@gmail.com>
2024-03-11 22:27:44 +0300

    added deleting riviews in deleted product

--- src/module/product/product.module.ts
+++ src/module/product/product.module.ts
@@ -3,10 +3,14 @@ import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ProductSchema } from './schema/product.schema';
+import { ReviewSchema } from '../review/schema/review.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: 'Products', schema: ProductSchema }]),
+    MongooseModule.forFeature([
+      { name: 'Products', schema: ProductSchema },
+      { name: 'Reviews', schema: ReviewSchema },
+    ]),
   ],
   controllers: [ProductController],
   providers: [ProductService],
