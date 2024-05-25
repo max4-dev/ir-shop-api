3c4e40e5ea97d8dfc068fa08b18bd2324524a4d5
Max <maksim.bogomyakov@gmail.com>
2024-05-25 03:11:12 +0300

    added parent adn chilren categories

--- src/module/category/dto/category.dto.ts
+++ src/module/category/dto/category.dto.ts
@@ -1,6 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class CategoryDto {
   @IsString()
   name: string;
+
+  @IsOptional()
+  @IsString()
+  parent: string;
 }
